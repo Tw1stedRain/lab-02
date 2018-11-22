e7ac1e0ed54d23956787ec0fb63ea6d77f0e47a5
Skyler Burger <skylerburger@gmail.com>
2018-11-21 18:40:26 -0800

    completed feature one

--- js/app.js
+++ js/app.js
@@ -10,10 +10,29 @@ function Horn(obj) {
 
 const allHorns = [];
 
+Horn.prototype.render = function () {
+  // creating new div and appending to main
+  $('main').append('<div class="entry"></div>');
+  // give new div a new js variable name so we can work with it
+  let $entry = $('div[class="entry"]');
 
+  // copy the tags from the html template
+  let hornTemplate = $('#photo-template').html();
+
+  // place the template tags into our new entry
+  $entry.html(hornTemplate);
+
+  // give our div content from the obj
+  $entry.find('h2').text(this.title);
+  $entry.find('img').attr('src', this.image_url);
+  $entry.find('p').text(this.description);
+
+  $entry.removeClass('entry');
+  $entry.attr('class', this.name);
+};
 
 function readJson () {
-  $.get('./page-1.json', 'json')
+  $.get('data/page-1.json', 'json')
     .then( data => {
       data.forEach( hornObj => {
         new Horn(hornObj);
@@ -27,3 +46,4 @@ function readJson () {
 }
 
 $(() => readJson());
+// readJson();
