98e4526b6d6fb7aafd34ab34d1892e759c59853c
Nicole Kalbfleisch <ni.ka.50@hotmail.com>
2018-11-21 19:28:56 -0800

    part one of feature 2 done

--- js/app.js
+++ js/app.js
@@ -26,10 +26,21 @@ Horn.prototype.render = function () {
   $entry.find('h2').text(this.title);
   $entry.find('img').attr('src', this.image_url);
   $entry.find('p').text(this.description);
+  $entry.find('h6').text(this.horns);
 
   $entry.removeClass('entry');
   $entry.attr('class', this.name);
 };
+
+Horn.prototype.menu = function () {
+  $('select').append('<option class = "option"></option>');
+  let $option = $('option[class="option"]');
+
+  $option.attr('value', this.keyword);
+  $option.text(this.keyword);
+
+  $option.removeClass('option');
+}
 
 function readJson () {
   $.get('data/page-1.json', 'json')
@@ -41,6 +52,7 @@ function readJson () {
     .then( () => {
       allHorns.forEach( horn => {
         horn.render();
+        horn.menu();
       });
     });
 }
